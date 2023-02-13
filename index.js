1aa670c4b7fdce17b0c92e3e35cc5dc224fc14b2
fmartinezfenoll <franmartinezfenoll@gmail.com>
2023-02-13 13:05:26 +0100

    Incorporación de Nodemon y Morgan

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict'
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 3000;
 const express = require('express');
+const logger = require('morgan');
 const app = express();
-app.get('/hola/:unNombre', (req, res) => {
-res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
+// Declaramos los middleware
+app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
+// Declaramos el API
+app.get('/hola/:name', (req, res) => {
+res.status(200).send({mensaje: `¡Hola ${req.params.name} desde SD con JSON!`});
 });
 app.listen(port, () => {
-console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
-});
+console.log(`API REST ejecutándose en http://localhost:${port}/hola/:nombre`);
+}); 
