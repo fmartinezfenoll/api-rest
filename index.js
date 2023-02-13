7bf932662dd8302e868a04ce64fe0c8ce0686cdc
fmartinezfenoll <franmartinezfenoll@gmail.com>
2023-02-13 15:43:16 +0100

    API REST CRUD (sin base de datos)

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 'use strict'
 const port = process.env.PORT || 3000;
+
 const express = require('express');
 const logger = require('morgan');
+
 const app = express();
-// Declaramos los middleware
-app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
-// Declaramos el API
-app.get('/hola/:name', (req, res) => {
-res.status(200).send({mensaje: `¡Hola ${req.params.name} desde SD con JSON!`});
+
+//middleware
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//declarar el API
+
+app.get('/api/producto', (req, res) => {
+    res.status(200);
+    res.send({ productos: []});
 });
+
+app.get('/api/producto/:productID', (req, res) => {
+    const productID = req.params.productID;
+    
+    res.status(200);
+    res.send({ producto: productID });
+});
+
+app.post('/api/producto', (req, res) => {
+    const queProducto = req.body;
+    console.log(queProducto);
+
+    res.status(200);
+    res.send({
+        mensaje: 'Producto creado',
+        producto: queProducto
+    });
+});
+
+app.put('/api/producto/:productID', (req, res) => {
+        const queProducto = req.body;
+        const productID = req.params.productID;
+        res.status(200);
+        res.send({
+        mensaje: `Producto ${productID} modificado`,
+        producto: queProducto
+    });
+});
+app.delete('/api/producto/:productID', (req, res) => {
+    const productID = req.params.productID;
+    res.status(200);
+    res.send( { mensaje: `Producto ${productID} eliminado` } );
+});
+
 app.listen(port, () => {
-console.log(`API REST ejecutándose en http://localhost:${port}/hola/:nombre`);
-}); 
+    console.log(`API REST ejecutándose en http://localhost:${port}/api/producto`);
+});
